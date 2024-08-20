2bb21a55d52628d7d8868399e1a74333d8915a11
Sandeep Tharu <sandeeptharu@Sandeeps-MacBook-Pro-2.local>
2024-08-20 07:48:54 +0545

    commented endpoint enum

--- Sources/Casdoor/Models/EndPointEnum.swift
+++ Sources/Casdoor/Models/EndPointEnum.swift
@@ -8,26 +8,26 @@
 import Foundation
 import AF
 
-public enum Endpoint{
-    case verficationCode(endpoint : String)
-    case getEmailAndPhone(email : String)
-    
-    var urlString : String{
-        switch self {
-        case .verficationCode(let endPoint):
-            return "\(endPoint)send-verification-code"
-        case .getEmailAndPhone(let endPoint,_):
-            return "\(endPoint)get-email-and-phone"
-        }
-    }
-    
-    var httpMethod : HTTPMethod{
-        switch self {
-        case .verficationCode:
-            return .post
-        case .getEmailAndPhone:
-            return .get
-        }
-    }
-    
-}
+//public enum Endpoint{
+//    case verficationCode(endpoint : String)
+//    case getEmailAndPhone(email : String)
+//    
+//    var urlString : String{
+//        switch self {
+//        case .verficationCode(let endPoint):
+//            return "\(endPoint)send-verification-code"
+//        case .getEmailAndPhone(let endPoint,_):
+//            return "\(endPoint)get-email-and-phone"
+//        }
+//    }
+//    
+//    var httpMethod : HTTPMethod{
+//        switch self {
+//        case .verficationCode:
+//            return .post
+//        case .getEmailAndPhone:
+//            return .get
+//        }
+//    }
+//    
+//}
